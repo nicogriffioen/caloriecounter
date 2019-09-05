f3e24c648ff9f5e2c236146b4fdff8c0343c9dfb
Nico Griffioen <nicogriffioen@gmail.com>
2019-09-05 09:39:33 +0200

    Add field date & time to DiaryEntry

--- caloriecounter/diary/admin.py
+++ caloriecounter/diary/admin.py
@@ -5,10 +5,10 @@ from caloriecounter.diary.models import DiaryEntry
 
 
 class DiaryEntryAdmin(admin.ModelAdmin):
+    readonly_fields = ['user', 'created_on', 'nutritional_information']
+    fields = ['user', 'created_on','unit', 'product', 'quantity', 'nutritional_information']
     list_display = ['__str__', 'user', 'created_on']
     autocomplete_fields = ['user', 'product', 'unit']
-
-
 
 
 admin.site.register(DiaryEntry, DiaryEntryAdmin)
--- caloriecounter/diary/models.py
+++ caloriecounter/diary/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.db import models
 from django.utils.translation import ugettext as _
 
@@ -11,21 +13,28 @@ class DiaryEntry(models.Model):
         verbose_name = _('diary entry')
         verbose_name_plural = ('diary entries')
 
-    user = models.ForeignKey(to=User, on_delete=models.CASCADE, blank=False)
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE, blank=False, editable=False)
 
-    created_on = models.DateTimeField(auto_now_add=True)
+    created_on = models.DateTimeField(auto_now_add=True, editable=False)
+
+    date = models.DateField(_("Entry Date"), blank=False, default=datetime.now)
+    time = models.TimeField(_("Entry Time"), blank=False, default=datetime.now)
 
     product = models.ForeignKey(to=FoodProduct, on_delete=models.CASCADE, blank=False)
     quantity = models.FloatField()
     unit = models.ForeignKey(to=Unit, on_delete=models.SET_NULL, null=True, blank=False)
 
-    def get_nutritional_information(self):
-        return ''
+    @property
+    def nutritional_information(self):
+        return {'energy' : '20 cals'}
+
+
 
     def clean(self, *args, **kwargs):
         super(DiaryEntry, self).clean(*args, **kwargs)
 
     def save(self, *args, **kwargs):
+        self.created_on = self.created_on.replace(tzinfo=None)
         super(DiaryEntry, self).save(*args, **kwargs)
 
     def __str__(self):
