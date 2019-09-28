2469fcaab3783a25a9ec3bf530fe3a2bfc80b5e9
Nico Griffioen <nicogriffioen@gmail.com>
2019-09-28 12:13:01 +0200

    Change to local and production settings and update dependencies

--- caloriecounter/settings/base.py
+++ caloriecounter/settings/base.py
@@ -27,9 +27,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-SITE_URL = "http://127.0.0.1:8000"
-
-
 # Application definition
 AUTH_USER_MODEL = 'user.User'
 
@@ -86,28 +83,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'caloriecounter.wsgi.application'
-
-# Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'caloriecounter',
-        'USER': 'caloriecounter',
-        'PASSWORD': 'caloriecounter',
-        'HOST': 'localhost',
-        'PORT': '',
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
--- caloriecounter/wsgi.py
+++ caloriecounter/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'caloriecounter.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'caloriecounter.settings.local')
 
 application = get_wsgi_application()
